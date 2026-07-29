ef2d365cfc14bc65e4291f55e64cdf8e77726799
dzikrnshdjafar <dzikriansahdjafar@gmail.com>
2026-07-29 16:27:23 +0800

    fix gamelist

--- gamelist.lua
+++ gamelist.lua
@@ -2,18 +2,7 @@ local Games = {
   [9098570654] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/d156a7428f1935c2.lua",
   [5750914919] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/8725f77bb892a3ad.lua",
   [10200395747] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/d7e2ace39ba9ad6c.lua",
-  [7326934954] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/99-nights-in-the-forest-combined.lua",
-  [6701277882] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/fish-it-combined.lua",
-  [6739698191] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/f9dd305c6ea188c1.lua",
-  [10051853469] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/be-flash-for-brainrots-combined.lua",
-  [994732206] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/blox-fruits-combined.lua",
-  [4777817887] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/blade-ball-combined.lua",
-  [9186719164] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/sailor.lua",
-  [10039338037] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/ebbc13ac68b965cc.lua",
-  [9348272796] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/330657024dd1b472.lua",
-  [10004244222] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/kick-a-lucky-block-combined.lua",
   [7436755782] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/12bc55cbc4d8b01b.lua",
   [129954712878723] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/06ebcb8a5d1a6d7b.lua",
-  [9792947201] = "https://raw.githubusercontent.com/dzikrnshdjafar/ponggo-hub/main/game-script/slime-rng-combined.lua",
 }
 return Games
